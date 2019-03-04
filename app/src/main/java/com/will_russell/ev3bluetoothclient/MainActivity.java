7e39bd51944052f36ede75f815f28bdebe7ddefc
Will Russell <wxr867@student.bham.ac.uk>
2019-03-04 16:20:49 +0000

    Get text from fields

--- app/src/main/java/com/will_russell/ev3bluetoothclient/MainActivity.java
+++ app/src/main/java/com/will_russell/ev3bluetoothclient/MainActivity.java
@@ -2,16 +2,40 @@ package com.will_russell.ev3bluetoothclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.io.*;
+import java.net.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listview = (ListView) findViewById(R.id.output_view);
     }
 
-    private void setup() {
+    private void getData(String ip, int port) throws IOException {
+        Socket socket = new Socket(ip, port);
+        InputStream in = socket.getInputStream();
+        DataInputStream dataIn = new DataInputStream(in);
+        String output = dataIn.readUTF();
+        TextView tv = new TextView(this);
+        tv.setText(output);
+        listview.addView(tv);
+    }
 
+    private void onClick(View v) {
+
+        TextInputEditText ipText = (TextInputEditText) findViewById(R.id.ip_box);
+        TextInputEditText portText = (TextInputEditText) findViewById(R.id.port_box);
+        getData(ipText.getText(), Integer.valueOf(portText.getText()));
     }
 }
