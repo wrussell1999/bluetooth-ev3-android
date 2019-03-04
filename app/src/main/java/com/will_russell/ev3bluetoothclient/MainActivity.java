87a6aaf86e39d09e250f83c4c0fdc51d021f459e
Will Russell <wxr867@student.bham.ac.uk>
2019-03-04 15:07:39 +0000

    Initial commit

---
+++ app/src/main/java/com/will_russell/ev3bluetoothclient/MainActivity.java
@@ -0,0 +1,17 @@
+package com.will_russell.ev3bluetoothclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void setup() {
+        
+    }
+}
