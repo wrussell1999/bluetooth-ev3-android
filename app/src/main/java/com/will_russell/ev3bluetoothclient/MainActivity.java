1c5a5eeb75dad02cca0f4720bb79eea0b7376b90
Will Russell <wxr867@student.bham.ac.uk>
2019-03-04 23:09:47 +0000

    Dialog box for connection

--- app/src/main/java/com/will_russell/ev3bluetoothclient/MainActivity.java
+++ app/src/main/java/com/will_russell/ev3bluetoothclient/MainActivity.java
@@ -1,11 +1,14 @@
 package com.will_russell.ev3bluetoothclient;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 import com.google.android.material.textfield.TextInputEditText;
+import android.app.AlertDialog;
 
 import java.io.*;
 import java.net.*;
@@ -13,6 +16,7 @@ import java.net.*;
 public class MainActivity extends AppCompatActivity {
 
     private ListView listview;
+    private AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listview = (ListView) findViewById(R.id.output_view);
+        builder = new AlertDialog.Builder(this);
     }
 
     private void getData(String ip, int port) throws IOException {
@@ -32,12 +37,28 @@ public class MainActivity extends AppCompatActivity {
         listview.addView(tv);
     }
 
-    private void onClick(View v) {
-
+    public void onClick(View v) {
         TextInputEditText ipText = (TextInputEditText) findViewById(R.id.ip_box);
         TextInputEditText portText = (TextInputEditText) findViewById(R.id.port_box);
+        if (ipText.getText().toString().trim().length() > 0) {
+            builder.setMessage("You need to enter an IP address");
+            AlertDialog alert = builder.create();
+            alert.setTitle("No IP Address");
+            alert.show();
+        } else if (portText.getText().toString().trim().length() > 0) {
+            builder.setMessage("You need to enter a Port number");
+            AlertDialog alert = builder.create();
+            alert.setTitle("No Port Number");
+            alert.show();
+        } else {
+            startConnection(ipText.getText().toString(), Integer.valueOf(portText.getText().toString()));
+        }
+
+    }
+
+    private void startConnection(String ip, int port) {
         try {
-            getData(ipText.getText().toString(), Integer.valueOf(portText.getText().toString()));
+            getData(ip, port);
         } catch (IOException e) {
             e.printStackTrace();
         }
